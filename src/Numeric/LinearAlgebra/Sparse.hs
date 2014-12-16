e8a96ffd97d39206f15375648147da5ce6f371eb
Thomas Tuegel <ttuegel@gmail.com>
2014-12-15 21:24:44 -0600

    mulV: implement matrix-vector multiplication

--- src/Numeric/LinearAlgebra/Sparse.hs
+++ src/Numeric/LinearAlgebra/Sparse.hs
@@ -4,6 +4,7 @@
 
 module Numeric.LinearAlgebra.Sparse where
 
+import Data.Foldable (forM_)
 import Data.Ord (comparing)
 import qualified Data.Vector.Algorithms.Intro as Intro
 import Data.Vector.Unboxed (Unbox, Vector)
@@ -14,7 +15,7 @@ import GHC.Stack (errorWithStackTrace)
 data Orient = Row | Col
 
 data Matrix (or :: Orient) a
-    = Matrix {-# UNPACK #-} !Int !(Vector Int) !(Vector Int) !(Vector a)
+    = Matrix {-# UNPACK #-} !Int !(Vector Int) !(Vector (Int, a))
 
 class OrientOf or where
     orientOf :: Matrix or a -> Orient
@@ -34,10 +35,10 @@ sortCoords = \case
 {-# INLINE sortCoords #-}
 
 countMajors :: Int -> Vector Int -> Vector Int
-countMajors dim ixs = V.create $ do
-    v <- MV.replicate 0 (dim + 1)
+countMajors dim_ ixs = V.create $ do
+    v <- MV.replicate 0 (dim_ + 1)
     V.forM_ ixs $ \ix ->
-        if ix >= dim
+        if ix >= dim_
           then errorWithStackTrace "index out of bounds"
           else MV.write v (ix + 1) . (+ 1) =<< MV.read v (ix + 1)
     return v
@@ -57,6 +58,45 @@ compress = \r c coords ->
           Row -> countMajors r rows
           Col -> countMajors c cols
         minorDim = case orient of { Row -> c; Col -> r }
-        compressed = Matrix minorDim majors minors entries
+        compressed = Matrix minorDim majors $ V.zip minors entries
     in compressed
 {-# INLINE compress #-}
+
+slices :: Unbox a => Matrix or a -> [(Int, Vector (Int, a))]
+slices = \(Matrix _ rows entries) -> do
+    i <- enumFromTo 0 $ V.length rows - 1
+    let start = rows V.! i
+        end = rows V.! (i + 1)
+    return $ (i, V.slice start (end - start) entries)
+{-# INLINE slices #-}
+
+dim :: OrientOf or => Matrix or a -> Int
+dim = \mat@(Matrix minorDim majors _) ->
+    case orientOf mat of
+      Row -> V.length majors - 1
+      Col -> minorDim
+{-# INLINE dim #-}
+
+codim :: OrientOf or => Matrix or a -> Int
+codim = \mat@(Matrix minorDim majors _) ->
+    case orientOf mat of
+      Col -> V.length majors - 1
+      Row -> minorDim
+{-# INLINE codim #-}
+
+mulV
+  :: (Num a, Unbox a)
+  => Matrix Row a
+  -> Vector a
+  -> Vector a
+mulV = \mat xs ->
+    if V.length xs /= dim mat
+      then errorWithStackTrace "matrix dimension does not match vector"
+      else V.create $ do
+          ys <- MV.replicate (V.length xs) 0
+          forM_ (slices mat) $ \(r, as) -> do
+              y <- MV.read ys r
+              let ax = V.sum $ flip V.map as $ \(c, a) -> a * (xs V.! c)
+              MV.write ys r $ ax + y
+          return ys
+{-# INLINE mulV #-}
