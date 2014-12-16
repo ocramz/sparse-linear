edbbc7a8fc3adb7d29e1bb90c50d6522f806f3bc
Thomas Tuegel <ttuegel@gmail.com>
2014-12-16 08:16:12 -0600

    Refactor with RecordWildCards

--- src/Numeric/LinearAlgebra/Sparse.hs
+++ src/Numeric/LinearAlgebra/Sparse.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Numeric.LinearAlgebra.Sparse where
 
-import Data.Foldable (forM_)
+import Data.Function (fix)
 import Data.Ord (comparing)
 import qualified Data.Vector.Algorithms.Intro as Intro
 import Data.Vector.Unboxed (Unbox, Vector)
@@ -14,8 +14,13 @@ import GHC.Stack (errorWithStackTrace)
 
 data Orient = Row | Col
 
-data Matrix (or :: Orient) a
-    = Matrix {-# UNPACK #-} !Int !(Vector Int) !(Vector (Int, a))
+data Matrix (or :: Orient) a = Matrix
+    { nmajor :: {-# UNPACK #-} !Int -- positive
+    , nminor :: {-# UNPACK #-} !Int -- positive
+    , majors :: !(Vector Int) -- length == nmajor + 1; head == 0
+    , minors :: !(Vector Int) -- length == nz; all (< nminor)
+    , entries :: !(Vector a) -- length == nz
+    }
 
 class OrientOf or where
     orientOf :: Matrix or a -> Orient
@@ -28,85 +33,100 @@ instance OrientOf Col where
     orientOf = \_ -> Col
     {-# INLINE orientOf #-}
 
-sortCoords :: Unbox a => Orient -> Vector (Int, Int, a) -> Vector (Int, Int, a)
-sortCoords = \case
-    Row -> V.modify $ Intro.sortBy $ comparing $ \(r, c, _) -> (r, c)
-    Col -> V.modify $ Intro.sortBy $ comparing $ \(r, c, _) -> (c, r)
-{-# INLINE sortCoords #-}
-
-countMajors :: Int -> Vector Int -> Vector Int
-countMajors dim_ ixs = V.create $ do
-    v <- MV.replicate 0 (dim_ + 1)
-    V.forM_ ixs $ \ix ->
-        if ix >= dim_
-          then errorWithStackTrace "index out of bounds"
-          else MV.write v (ix + 1) . (+ 1) =<< MV.read v (ix + 1)
-    return v
-{-# INLINE countMajors #-}
-
 compress
   :: (OrientOf or, Unbox a)
   => Int
   -> Int
   -> Vector (Int, Int, a)
   -> Matrix or a
-compress = \r c coords ->
+compress = \nr nc coords -> fix $ \compressed ->
     let orient = orientOf compressed
-        (rows, cols, entries) = V.unzip3 $ sortCoords orient coords
+        nmajor = case orient of { Row -> nr; Col -> nc }
+        nminor = case orient of { Row -> nc; Col -> nr }
+
+        comparison = case orient of
+          Row -> comparing $ \(r, c, _) -> (r, c)
+          Col -> comparing $ \(r, c, _) -> (c, r)
+        sorted = V.modify (Intro.sortBy comparison) coords
+        (rows, cols, entries) = V.unzip3 sorted
+
         minors = case orient of { Row -> cols; Col -> rows }
+        count ixs = V.create $ do
+            v <- MV.replicate 0 (nmajor + 1)
+            V.forM_ ixs $ \ix ->
+                if ix >= nmajor
+                  then errorWithStackTrace "major index out of bounds"
+                  else MV.write v (ix + 1) . (+ 1) =<< MV.read v (ix + 1)
+            return v
         majors = case orient of
-          Row -> countMajors r rows
-          Col -> countMajors c cols
-        minorDim = case orient of { Row -> c; Col -> r }
-        compressed = Matrix minorDim majors $ V.zip minors entries
-    in compressed
+          Row -> count rows
+          Col -> count cols
+    in Matrix{..}
 {-# INLINE compress #-}
 
-slices :: Unbox a => Matrix or a -> [(Int, Vector (Int, a))]
-slices = \(Matrix _ rows entries) -> do
-    i <- enumFromTo 0 $ V.length rows - 1
-    let start = rows V.! i
-        end = rows V.! (i + 1)
-    return $ (i, V.slice start (end - start) entries)
-{-# INLINE slices #-}
+slice :: Unbox a => Int -> Matrix or a -> Vector (Int, a)
+slice n Matrix{..}
+  | n >= nmajor = errorWithStackTrace "major index out of bounds"
+  | otherwise =
+      let start = majors V.! n
+          end = majors V.! (n + 1)
+          len = end - start
+          minors' = V.slice start len minors
+          entries' = V.slice start len entries
+      in V.zip minors' entries'
+{-# INLINE slice #-}
 
 dim :: OrientOf or => Matrix or a -> Int
-dim = \mat@(Matrix minorDim majors _) ->
+dim mat@Matrix{..} =
     case orientOf mat of
-      Row -> V.length majors - 1
-      Col -> minorDim
+      Row -> nmajor
+      Col -> nminor
 {-# INLINE dim #-}
 
 codim :: OrientOf or => Matrix or a -> Int
-codim = \mat@(Matrix minorDim majors _) ->
+codim mat@Matrix{..} =
     case orientOf mat of
-      Col -> V.length majors - 1
-      Row -> minorDim
+      Col -> nmajor
+      Row -> nminor
 {-# INLINE codim #-}
+
+forI :: Monad m => Int -> (Int -> m ()) -> m ()
+forI end act = forI_go 0 where
+  forI_go n
+    | n < end = act n >> forI_go (n + 1)
+    | otherwise = return ()
+{-# INLINE forI #-}
+
+zipWithI
+  :: (Unbox a, Unbox b, Unbox c)
+  => (a -> b -> c) -> Vector (Int, a) -> Vector b -> Vector c
+zipWithI f ias bs = V.map (\(i, a) -> f a (bs V.! i)) ias
+{-# INLINE zipWithI #-}
 
 mulV
   :: (Num a, Unbox a)
   => Matrix Row a
   -> Vector a
   -> Vector a
-mulV = \mat xs ->
-    if V.length xs /= dim mat
-      then errorWithStackTrace "matrix dimension does not match vector"
-      else V.create $ do
-          ys <- MV.replicate (V.length xs) 0
-          forM_ (slices mat) $ \(r, as) -> do
-              y <- MV.read ys r
-              let ax = V.sum $ flip V.map as $ \(c, a) -> a * (xs V.! c)
-              MV.write ys r $ ax + y
-          return ys
+mulV mat@Matrix{..} xs
+  | V.length xs /= dim mat =
+      errorWithStackTrace "matrix dimension does not match vector"
+  | otherwise = V.create $ do
+      ys <- MV.replicate (V.length xs) 0
+      forI nmajor $ \r -> do
+          let ax = V.sum $ zipWithI (*) (slice r mat) xs
+          MV.write ys r . (+ ax) =<< MV.read ys r
+      return ys
 {-# INLINE mulV #-}
 
 fromDiag
   :: (Unbox a)
   => Vector a
   -> Matrix or a
-fromDiag = \xs ->
-    let dim_ = V.length xs
-        pairs = V.indexed xs
-    in Matrix dim_ (V.enumFromN 0 $ dim_ + 1) pairs
+fromDiag = \entries ->
+    let nmajor = V.length entries
+        nminor = nmajor
+        majors = V.enumFromN 0 (nmajor + 1)
+        minors = V.enumFromN 0 nminor
+    in Matrix{..}
 {-# INLINE fromDiag #-}
